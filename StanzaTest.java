b8414521d3b75e1c6307a3aa80112ab41ad4e2c6
SilviaCarriero <silvia.carriero28@gmail.com>
2026-04-05 19:29:26 +0200

    Update StanzaTest.java

--- StanzaTest.java
+++ StanzaTest.java
@@ -1,36 +1,76 @@
-package HomeworkA;
-
 import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeEach;
 
 import org.junit.jupiter.api.Test;
 
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
 class StanzaTest {
-	//Definisco qui i privati in modo da non ripeterli ogni volta
-		private Stanza stanza;
-		private Stanza stanzaAdicente;
-		private Attrezzo attrezzo;
-		
-		//Definisco con BeforeEach e setUp cosa deve essere eseguito prima di ogni test
-		
-		@BeforeEach
-		public void setUp() {
-			stanza=new Stanza("n11");
-			stanzaAdiacente = new Stanza("n12");
-			attrezzo=new Attrezzo("falce", 4);
 
+	//TEST HAS ATTREZZO
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testHasAttrezzo_stanzaVuota() {
+		Stanza vuota = new Stanza("vuota");
+		assertFalse(vuota.hasAttrezzo("martello"));
 	}
-
+	@Test
+	void testHasAttrezzo_trovato() {
+		Stanza piena = new Stanza ("piena"); //tasto destro su new stanza piena, refactor, create new variable 
+		piena.addAttrezzo(new Attrezzo("martello",10));
+		assertTrue(piena.hasAttrezzo("martello"));
+	}
+	@Test
+	void testHasAttrezzo_mancante() {
+		Stanza piena = new Stanza ("piena");
+		piena.addAttrezzo(new Attrezzo("martello",10));
+		assertFalse(piena.hasAttrezzo("introvabile"));
+	}
+	
+	//TEST ADD ATTREZZO
+	@Test
+	void testAddAttrezzo_null() {
+		Stanza s = new Stanza ("s");
+		assertTrue(s.addAttrezzo(null)); //va bene?
+	}
+	@Test
+	void testAddAttrezzo_funzionante() {
+		Stanza s = new Stanza ("s");
+		Attrezzo a =new Attrezzo("a",2);
+		assertTrue(s.addAttrezzo(a));
+	}
+	@Test
+	void testAddAttrezzo_hasAttrezzo() {
+		Stanza s = new Stanza ("s");
+		Attrezzo a =new Attrezzo("a",2);
+		s.addAttrezzo(a);
+		assertTrue(s.hasAttrezzo("a"));
+	}
+	@Test
+	void testAddAttrezzo_numeroMassimoRaggiunto() {
+		final Stanza piena=new Stanza("piena");
+		for(int i=0; i<10; i++) {
+			assertTrue(piena.addAttrezzo(new Attrezzo("attrezzo "+i, i)));
+		}
+		assertFalse(piena.addAttrezzo(new Attrezzo("escluso", 1)));
+	}
+	
+	//TEST REMOVE ATTREZZO
+	@Test
+	void testRemoveAttrezzo_null() {
+		Stanza s = new Stanza ("s");
+		assertFalse(s.removeAttrezzo(null)); //va bene?
+	}
+	@Test
+	void testRemoveAttrezzo_aggiunto() {
+		Stanza s = new Stanza ("s");
+		Attrezzo a =new Attrezzo("a",2);
+		s.addAttrezzo(a);
+		assertTrue(s.removeAttrezzo(a));
+	}
+	@Test
+	void testRemoveAttrezzo_stanzaVuota() {
+		Stanza s = new Stanza ("s");
+		Attrezzo a =new Attrezzo("a",2);
+		assertFalse(s.removeAttrezzo(a));
+	}
 }
-
-
-
-
-
-
-
-
